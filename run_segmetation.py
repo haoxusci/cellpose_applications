1b5ed51df1af1cc3e47503e95df1b17a17819f1f
Hao Xu <xuhaonju@gmail.com>
2021-01-19 16:05:04 +0100

    clean up the coding

--- run_segmetation.py
+++ run_segmetation.py
@@ -4,8 +4,8 @@ from covid_screening_quantify import quantify_all_fovs, combine_all_quantify
 
 if __name__ == "__main__":
     """run image segmentation and quantification"""
-    image_folder = "/home/haoxu/data/Images"
+    image_folder = "/home/haoxu/data/Images_10X/Images"
     quantify_all_fovs(image_folder)
-    processed_folder = "/home/haoxu/data/Images_processed"
+    processed_folder = image_folder + "_processed"
 
     combine_all_quantify(processed_folder)
